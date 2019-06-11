fb30b0c915312ad42e300e2018f10210cb9a24fa
jelleenet <mpknowles87@gmail.com>
2019-06-11 12:52:28 +0100

    Basic flipping card grid

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 window.Vue = require('vue');
 
+import GameCard from './components/GameCard.vue';
+
 const app = new Vue({
     el: '#app',
     components: {
+        GameCard,
     },
     data: {
         test : 'hello world'
---
+++ resources/js/components/GameCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="game-card" tabindex="0">
+        <div class="game-card__inner">
+            <div class="game-card__front">
+
+            </div>
+            <div class="game-card__back">
+                hi
+            </div>
+        </div>
+    </div>   
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+            }
+        }
+    }
+</script>
